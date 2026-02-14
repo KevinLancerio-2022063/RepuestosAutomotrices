2ea88469e0ab6f0ef7950faa95eb0d79fd53973a
KevinLancerio-2022063 <klancerio-2022063@kinal.edu.gt>
2026-02-13 18:21:10 -0600

    Feat: Se ha agregado la clase EmpleadoService

---
+++ src/main/java/com/kevinlancerio/RepuestosAutomotrices/service/EmpleadoService.java
@@ -0,0 +1,16 @@
+package com.kevinlancerio.RepuestosAutomotrices.service;
+
+import com.kevinlancerio.RepuestosAutomotrices.entity.Empleado;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface EmpleadoService {
+    List<Empleado> getAllEmpleados();
+    Empleado getEmpleadosById(Integer id);
+    Empleado saveEmpleado (Empleado empleado) throws RuntimeException;
+    Empleado updateEmpleado (Integer id, Empleado empleado);
+    void deleteEmpleado (Integer id);
+
+}
